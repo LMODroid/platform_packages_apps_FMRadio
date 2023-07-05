f2b433caa92a170a88c537ac77f301a41541fcb1
nift4 <nift4@protonmail.com>
2023-07-05 21:44:04 +0200

    fixup! FMRadio: fix listen to recordings
    * use runtime permissions
    
    Change-Id: Ie3ebb577483e5aec6c5dfbddda4d361ac5287fb5

--- provider/src/com/android/fmradio/recordings/PlayRecording.java
+++ provider/src/com/android/fmradio/recordings/PlayRecording.java
@@ -17,8 +17,10 @@ package com.android.fmradio.recordings;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.core.content.FileProvider;
 
@@ -26,15 +28,48 @@ import java.io.File;
 
 public class PlayRecording extends Activity {
 
+    private static final int REQUEST_CODE = 1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent oldIntent = getIntent();
 
+        boolean hasPermissionAudio = checkSelfPermission(android.Manifest.permission.READ_MEDIA_AUDIO) == PackageManager.PERMISSION_GRANTED;
+        boolean hasPermissionVideo = checkSelfPermission(android.Manifest.permission.READ_MEDIA_VIDEO) == PackageManager.PERMISSION_GRANTED;
+        boolean shouldShowRationale = false;
+        if (!hasPermissionAudio) {
+            shouldShowRationale |= shouldShowRequestPermissionRationale(android.Manifest.permission.READ_MEDIA_AUDIO);
+        }
+        if (!hasPermissionVideo) {
+            shouldShowRationale |= shouldShowRequestPermissionRationale(android.Manifest.permission.READ_MEDIA_VIDEO);
+        }
+
+        if (hasPermissionAudio && hasPermissionVideo) {
+            startPlayerNow();
+            return;
+        } else if (shouldShowRationale) {
+            Toast.makeText(this, R.string.info_toast, Toast.LENGTH_LONG).show();
+        }
+
+        requestPermissions(new String[] {
+            android.Manifest.permission.READ_MEDIA_AUDIO,
+            android.Manifest.permission.READ_MEDIA_VIDEO
+        }, REQUEST_CODE);
+    }
+
+    private void startPlayerNow() {
+        if (checkSelfPermission(android.Manifest.permission.READ_MEDIA_AUDIO) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(android.Manifest.permission.READ_MEDIA_VIDEO) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, R.string.denied_toast, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        final Intent oldIntent = getIntent();
         if (oldIntent != null && oldIntent.hasExtra("type") && oldIntent.hasExtra("path")) {
             Intent nextIntent = new Intent(Intent.ACTION_VIEW);
             Uri uri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName(),
-                    new File(Uri.parse(oldIntent.getStringExtra("path")).getPath()));
+                                                 new File(Uri.parse(oldIntent.getStringExtra("path")).getPath()));
             nextIntent.setDataAndType(uri, oldIntent.getStringExtra("type"));
             nextIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             startActivity(nextIntent);
@@ -42,4 +77,15 @@ public class PlayRecording extends Activity {
 
         finish();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                // If not granted, we show toast, so just go for it whatever happens
+                startPlayerNow();
+                break;
+        }
+   }
 }
